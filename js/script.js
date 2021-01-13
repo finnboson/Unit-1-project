58e30c0cc44bd553ba1a39a4736e87e7184b23d0
finnboson <finneganlease@gmail.com>
2021-01-13 07:57:29 -0600

    printQuote function done

--- js/script.js
+++ js/script.js
@@ -12,19 +12,31 @@ project 1 - A Random Quote Generator
 ***/
 let quotes = [
   {quoteText: 'When someone shows you who they are, believe them the first time.',
-   author: 'Maya Angelou',
+   source: 'Maya Angelou',
      },
   {quoteText: 'Seeing is not always believing.',
-   author: 'Martin Luther King, Jr.',
+   source: 'Martin Luther King, Jr.',
     },
   {quoteText: 'No person is your friend who demands your silence, or denies your right to grow.',
-   author: 'Alice Walker',
+   source: 'Alice Walker',
      },
   {quoteText: 'Your silence will not proctect you.',
-   author: 'Audre Lorde',
+   source: 'Audre Lorde',
      },
   {quoteText: 'Grab the broom of anger and drive off the beast of fear.',
-   author: 'Zora Neale Hurston',
+   source: 'Zora Neale Hurston',
+  },
+  {quoteText: `I feel that if we don't take seriously the ways in which racism is embedded in structures 
+               of institutions, if we assume that there must be an identifiable racist who is the perpetrator, 
+               then we won't ever succeed in eradicating racism.`,
+  source: 'Angela Y. Davis',
+  citation: 'Freedom is a Constant Struggle'
+  },
+  {quoteText: `I feel that if we don't take seriously the ways in which racism is embedded in structures 
+  of institutions, if we assume that there must be an identifiable racist who is the perpetrator, 
+  then we won't ever succeed in eradicating racism.`,
+source: 'Angela Y. Davis',
+citation: 'Freedom is a Constant Struggle'
   }
 ];
 
@@ -35,14 +47,12 @@ let quotes = [
 const upper = quotes.length;
 let chosenQuote = '';
 
-
-
 function getRandomQuote () {
   function getRandomNumber (upper) { 
     return Math.floor(Math.random() * upper);
   }
    chosenQuote = quotes[getRandomNumber(upper)];
-   return `${chosenQuote['quoteText']} ${chosenQuote['author']}`
+   return `${chosenQuote['quoteText']} ${chosenQuote['source']} ${chosenQuote['citation']}`
   }
   
 
@@ -52,12 +62,19 @@ console.log(getRandomQuote());
 /***
  * `printQuote` function
 ***/
+function printQuote () {
+  getRandomQuote();
+  let html = `<p class="quote">${chosenQuote['quoteText']}</p>
+              <p class="source">${chosenQuote['source']}</p> 
+              <span class="citation">${chosenQuote['citation']}</span>`;
+  return document.querySelector('div').innerHTML = html;
+}
 
-
+printQuote();
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-//document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
